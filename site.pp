524a4bc388eb66b249fb345b58bbd3d4168e2f41
Johan Sydseter <johan.sydseter@startsiden.no>
2013-02-27 23:50:11 +0100

    initial commit

---
+++ site.pp
@@ -0,0 +1,7 @@
+# Author: Johan Sydseter, <johan.sydseter@startsiden.no>
+# File resources are not refreshed if nodes are globed into site.pp
+# import "nodes/*.pp"
+
+# import nodes
+import "base/basenode.pp"
+import "nodes/selenium.startsiden.no.pp"
---
+++ t/01.dummy.t
@@ -0,0 +1,5 @@
+use Test::More;
+
+ok(1, 'just a dummy test');
+
+done_testing();
